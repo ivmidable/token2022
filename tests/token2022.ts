404dbd7214d2f10c708dd5fe135627c7006fcc7f
Richard Vade <ivmidable@gmail.com>
2023-03-15 08:10:07 -0400

    correct way to mint in web3

--- tests/token2022.ts
+++ tests/token2022.ts
@@ -1,10 +1,9 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, web3 } from "@project-serum/anchor";
 import { Token2022 } from "../target/types/token2022";
 import { TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, 
-  MINT_SIZE, getMinimumBalanceForRentExemptMint, 
+  getMintLen, ExtensionType, getMinimumBalanceForRentExemptMint, 
   NON_TRANSFERABLE_SIZE, createInitializeNonTransferableMintInstruction, createMint, createInitializeMint2Instruction } from "@solana/spl-token";
-
 
 
 describe("token2022", () => {
@@ -14,12 +13,11 @@ describe("token2022", () => {
   const connection = anchor.getProvider().connection;
   const program = anchor.workspace.Token2022 as Program<Token2022>;
 
-  const mint = anchor.web3.Keypair.generate();
-  const payer = anchor.web3.Keypair.generate();
+  const mint = web3.Keypair.generate();
+  const payer = web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    let res = await connection.requestAirdrop(payer.publicKey, 100 * anchor.web3.LAMPORTS_PER_SOL);
+  before(async () => {
+    let res = await connection.requestAirdrop(payer.publicKey, 100 * web3.LAMPORTS_PER_SOL);
 
     let latestBlockHash = await connection.getLatestBlockhash()
 
@@ -28,30 +26,13 @@ describe("token2022", () => {
       lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
       signature: res,
     });
+  });
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    
     //create the account
-    const lamports = await getMinimumBalanceForRentExemptMint(connection);
-
-    const transaction = new anchor.web3.Transaction().add(
-      anchor.web3.SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: mint.publicKey,
-        space: NON_TRANSFERABLE_SIZE,
-        lamports,
-        programId: TOKEN_2022_PROGRAM_ID,
-      })
-    );
-
-    transaction.add(
-      createInitializeNonTransferableMintInstruction(
-        mint.publicKey, TOKEN_2022_PROGRAM_ID
-      )
-    );
-
-    transaction.add(
-      createInitializeMint2Instruction(
-        mint.publicKey, 6, payer.publicKey, null, TOKEN_2022_PROGRAM_ID
-      )
-    );
+    
 
 
 
@@ -64,13 +45,43 @@ describe("token2022", () => {
 
     transaction.add(ix);
     */
+   let tx = await createNonTransferableMint(payer, connection, mint);
 
     try {
-      let tx = await anchor.web3.sendAndConfirmTransaction(connection, transaction, [payer, mint]);
-      console.log("Your transaction signature", tx);
+      let txid = await web3.sendAndConfirmTransaction(connection, tx, [payer, mint]);
+      console.log("Your transaction signature", txid);
     } catch (err) {
       console.log(err);
     }
 
   });
 });
+
+async function createNonTransferableMint(payer: web3.Keypair, connection: web3.Connection, mint: web3.Keypair  | undefined) {
+  const mintLength = getMintLen([ExtensionType.NonTransferable]);
+    const lamports = await connection.getMinimumBalanceForRentExemption(mintLength);
+
+    const tx = new anchor.web3.Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: mint.publicKey,
+        space:  mintLength,
+        lamports,
+        programId: TOKEN_2022_PROGRAM_ID,
+      })
+    );
+
+    tx.add(
+      createInitializeNonTransferableMintInstruction(
+        mint.publicKey, TOKEN_2022_PROGRAM_ID
+      )
+    );
+
+    tx.add(
+      createInitializeMint2Instruction(
+        mint.publicKey, 6, payer.publicKey, null, TOKEN_2022_PROGRAM_ID
+      )
+    );
+
+    return tx;
+}
